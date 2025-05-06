9806ba2b50546cb81ec2361ea58903e4ac6845f2
Anup patel <anuppatel78472@gmail.com>
2025-05-06 17:48:44 +0530

    Time: 7 ms (50.32%) | Memory: 42 MB (57.72%) - LeetSync

---
+++ 409-longest-palindrome/longest-palindrome.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        HashMap<Character, Integer> freqMap = new HashMap<>();
+        for (char c : s.toCharArray()) {
+            freqMap.put(c, freqMap.getOrDefault(c, 0) + 1);
+        }
+
+        int length = 0; int temp=0;
+        boolean oddFound = false;
+
+        for (int val : freqMap.values()) {
+            if (val % 2 == 0) {
+                length += val;
+            } else {
+                length += val - 1; 
+                
+            }
+        
+
+        if (val%2==1) {
+            if(temp<=0) temp++;
+        } } 
+        return length+temp;}
+}
